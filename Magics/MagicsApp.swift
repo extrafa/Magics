897dc496634448ce014316bef34fb2dfe353d516
extrafa <rostislavpredein@gmail.com>
2025-11-07 22:41:29 -0300

    Initial Commit

---
+++ Magics/MagicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicsApp.swift
+//  Magics
+//
+//  Created by Ross on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MagicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
